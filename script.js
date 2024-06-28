8b1c2a413cc73c287d8450d9a2cc32075d268865
Classxx <foflard1337@gmail.com>
2024-06-29 00:16:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function windowOnClick(event) {
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+
+
+
+document.querySelector('#gameslist').oninput = function(){
+    let val = this.value.trim();
+    let gameslistItems = document.querySelectorAll('.gameslist li');
+    if (val != ''){
+        gameslistItems.forEach(function(elem){
+            if (elem.innerText.search(val) == -1){
+                elem.classList.add('hide');
+            }
+            else {
+                elem.classList.remove('hide');
+            }
+        });
+
+    }
+    else {
+        gameslistItems.forEach(function(elem){
+            elem.classList.remove('hide');
+        });
+    }
+}
+
+
+
+
