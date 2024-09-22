7ccbc8192ee4a468eb73119d5c9b7c8c189e9730
Classxx <foflard1337@gmail.com>
2024-09-22 10:25:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const tele = window.Telegram.WebApp;
-tele.expand();
-
+let tg = window.Telegram;
+  
+if(tg != undefined){
+  if (tg.WebApp != undefined && tg.WebApp.initData != undefined);
+   
+  let safe = tg.WebApp.initData;
+  
+  tg.WebApp.backgroundColor = '#3d3d3d';
+  tg.WebApp.headerColor = '#212121';
+  tg.WebApp.expand(); }
+//-----------------------------------------------------------------------------------------
 var modal = document.querySelector(".modal");
 var trigger = document.querySelector(".trigger");
 var closeButton = document.querySelector(".close-button");
